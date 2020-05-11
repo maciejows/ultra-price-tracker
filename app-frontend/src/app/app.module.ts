d05ae7e506ecd3418a83a283f6b6bc4eb9ca2b28
maciejows <owsianny.maciej@gmail.com>
2020-05-11 12:02:36 +0200

    Initialized MDBootstrap for Angular

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MDBBootstrapModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
