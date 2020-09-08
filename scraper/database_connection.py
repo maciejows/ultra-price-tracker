a0a0e69d16c44eec593c00d3f53d001b71c8a9e3
maciejows <owsianny.maciej@gmail.com>
2020-09-08 17:35:31 +0200

    Added serialization for objects - now u can pass item object to add it to database

--- scraper/database_connection.py
+++ scraper/database_connection.py
@@ -2,23 +2,16 @@ from pymongo import MongoClient
 from scraper.models.Item import Item
 from scraper.models.Shop import Shop
 from scraper.models.Data import Data
-from scraper.webDrvr import *
-import datetime
 
 
 cluster = MongoClient("mongodb+srv://xyz:1234@projektti-5lupj.mongodb.net/scraper?retryWrites=true&w=majority")
 db = cluster["scraper"]
 ogolna = db["Ogolna"]
 
+item1 = Item("Maciej koks2")
+item2 = Item("Apple iPhone X Plus 64GB")
 
-data1 = Data(1241324, "01-01-2020")
-data2 = Data(7899, "01-03-2020")
-data3 = Data(7500, "01-05-2020")
-shop1 = Shop("komputronik", "www.komputronik.pl/iphone-7-elo", data1)
-shop2 = Shop("morele", "www.morele.pl//apple-iPhone-X-Plus-64GB", data2)
-shop3 = Shop("x-kom", "www.x-kom.pl", data3)
-item1 = Item("Apple iPhone 7 Plus", shop2)
-item2 = Item("Apple iPhone X Plus 64GB", shop3)
+item1.logItem()
 
 
 def doesUrlExist(url):
@@ -48,13 +41,7 @@ def addItemToDatabase(item):
             id_ = 0
         else:
             id_ = x["_id"] + 1
-        item_frame = {"_id": id_, "item_name": item.getName(), "shops": [{"shop_name": "komputronik", "url": "Null", "data": []},
-                                                                      {"shop_name": "morele", "url": "Null", "data": []},
-                                                                      {"shop_name": "media-markt", "url": "Null", "data": []},
-                                                                      {"shop_name": "media-expert", "url": "Null", "data": []},
-                                                                      {"shop_name": "euro", "url": "Null", "data": []},
-                                                                      {"shop_name": "x-kom", "url": "Null", "data": []},
-                                                                      {"shop_name": "neo24", "url": "Null", "data": []}]}
+        item_frame = {"_id": id_, **item.serialize}
         ogolna.insert_one(item_frame)
     else:
         return
@@ -78,17 +65,8 @@ def setShopUrl(item, shop):
 def addDateToShop(item, shop, data):
     ogolna.find = {"item_name": item.getName()}
     find = {"shops.shop_name": shop.getName()}
-    x = datetime.datetime.now()
-    newData = {"$addToSet": {"shops.$.data": {"price": data.getPrice(), "date": x.strftime("%d-%m-%Y")}}}
+    newData = {"$addToSet": {"shops.$.data": Data()}}
     ogolna.update_one(find, newData)
-
-
-#raczej do wywalenia
-def addShopToItem(item, shop):
-    find = {"item_name": item.getName()}
-    shop = {"shop_name": shop.getName(), "url": shop.getURL()}
-    newShop = {"$addToSet": {"shops": shop}}
-    ogolna.update_one(find, newShop)
 
 
 def getShopUrl(item_name, shop_name):
@@ -115,11 +93,11 @@ def getShopData(item_name, shop_name):
 
 
 if __name__ == "__main__":
-    # addItemToDatabase(item1)
+      addItemToDatabase(item1)
     # addItemToDatabase(item2)
     # setShopUrl(item1, shop1)
     # setShopUrl(item2, shop2)
-    addDateToShop(item1, shop2, data1)
+    # addDateToShop(item1, shop2, data1)
     # addDateToShop(item2, shop2, data3)
     # doesUrlExist("www.x-kom.pl")
     # doesNameExist("Apple iPhone X Plus")
--- scraper/models/Shop.py
+++ scraper/models/Shop.py
@@ -1,9 +1,11 @@
-class Shop:
+from scraper.models.Data import Data
 
+
+class Shop:
     def __init__(self, name, url, data):
         self.name = name
         self.url = url
-        self.data = [data]
+        self.data = data
 
     def setName(self, name):
         self.name = name
@@ -19,3 +21,14 @@ class Shop:
 
     def getURL(self):
         return self.url
+
+    @property
+    def serialize(self):
+        data_serialized = []
+        for data in self.data:
+            data_serialized.append(data.serialize)
+        return {
+            "shop_name": self.name,
+            "url": self.url,
+            "data": data_serialized
+        }
