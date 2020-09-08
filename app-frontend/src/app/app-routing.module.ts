861ec5a591dc919131d6bbf2bf32eb6a0ccd950d
maciejows <owsianny.maciej@gmail.com>
2020-09-08 14:47:12 +0200

    Removed unused functionality

--- app-frontend/src/app/app-routing.module.ts
+++ app-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ItemSelectComponent } from './components/item-select/item-select.component';
 
 const routes: Routes = [
-  { path: 'search', component: ItemSelectComponent },
   { path: '**', redirectTo: '/' } 
 ];
 
